e44f009f5751e9d7d67bb5fddd8c72d5a50a4394
kolserdav <serega12101983@gmail.com>
2018-04-07 22:41:09 +0800

    work-vers

---
+++ README.md
@@ -0,0 +1,24 @@
+Generator pseudo name. 
+```
+Firstname{3-12}_Lastname{3-12};
+```
+_Install_
+
+~$ `npm i pseudo-name`
+
+Using
+
+```javascript
+let name = require('pseudo-name');
+let fullPseudoName = name();
+```
+
+Random Results
+
+```
+Upetepo_Xifisywuzof
+Izuniz_Ofigy
+Lozycumesew_Bikyto
+```
+
+It works...
---
+++ index.js
@@ -0,0 +1,83 @@
+'use strict';
+
+module.exports = fu;
+
+/**
+ *
+ * @returns {null}|{h.fullName}
+ */
+function fu()
+{
+    /**
+     *
+     * @constructor
+     */
+    function H(){}
+    H.prototype.name = null;
+    H.prototype.firstName = null;
+    H.prototype.lastName = null;
+    H.prototype.fullName = null;
+    let h = new H();
+
+    /**
+     *
+     * @type {string}
+     */
+    let uppercases = 'QWERTYUIOPASDFGHJKLMNBVCXZ';
+    let vovels = /[EYUIOAeyuioa]/;
+    let symVovels = 'eyuioa';
+    let symConsonans = 'qwrtpsdfghjklzxcvbnm';
+
+    /**
+     *
+     * @type {string}
+     */
+    let lengthName = (Math.random()*12).toFixed(0);
+    lengthName = (lengthName < 3)? 6 : lengthName;
+
+    /**
+     * Name generator
+     */
+    getName();
+
+    return h.fullName;
+
+    /**
+     *
+     * @param i
+     * @returns {null}
+     */
+    function getName(i = 0) {
+
+        if (i < lengthName){
+            if (h.name === null){
+                h.name = uppercases[(Math.random()*(uppercases.length - 1)).toFixed(0)];
+                return getName(i +1);
+            }
+            else {
+                if (h.name[i -1].match(vovels)){
+                    h.name += symConsonans[(Math.random()*(symConsonans.length - 1)).toFixed(0)];
+                    return getName(i + 1);
+                }
+                else{
+                    h.name += symVovels[(Math.random()*(symVovels.length - 1)).toFixed(0)];
+                    return getName(i + 1);
+                }
+            }
+        }
+        else {
+            if (h.firstName === null){
+                h.firstName = h.name;
+                h.name = null;
+                lengthName = (Math.random()*12).toFixed(0);
+                lengthName = (lengthName < 3)? 6 : lengthName;
+                return getName();
+            }
+            if (h.lastName === null && h.firstName !== null) {
+                h.lastName = h.name;
+                h.fullName = h.firstName +'_' +h.lastName;
+            }
+        }
+        return h.fullName;
+    }
+}
